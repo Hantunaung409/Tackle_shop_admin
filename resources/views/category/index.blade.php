4baa583a278ad58f5e8daa069fb4d71053551d67
Hantunaung409 <hanhtunaung409@gmail.com>
2022-11-29 23:51:28 +0630

    before yajra dataTabel

--- resources/views/category/index.blade.php
+++ resources/views/category/index.blade.php
@@ -1,9 +1,8 @@
 @extends('mainLayout')
 @section('content')
             <div class="row bg-light m-1">
-            <div class="container2 ">
                 <div class="row mt-2">
-                    <div class="col-4">
+                    <div class="col-12 col-sm-4 col-md-4 col-lg-4 col-xl-4 col-xxl-4">
                         <!-- add new category Card -->
                         <div class="card shadow-sm bg-white text-center">
                             <div class="card-title mt-2">Add A New Category</div>
@@ -23,11 +22,11 @@
                         </div>
                         <!-- end of add new category card -->
                     </div>
-                    <div class="col-8 ps-5">
+                    <div class="col-12 col-sm-8 col-md-8 col-lg-8 col-xl-8 col-xxl-8 ps-5 mt-5 mt-sm-0 mt-lg-0 mt-xl-0 mt-xxl-0">
                         <table class=" table table-striped shadow-sm rounded">
                             <thead>
                                 <tr>
-                                    <th>id</th>
+                                    <th>Id</th>
                                     <th>Name</th>
                                     <th>Note</th>
                                     <th></th>
@@ -35,9 +34,12 @@
                                 </tr>
                             </thead>
                             <tbody>
+                                <input type="hidden" name="currentPage" id="currentPage" value="{{ $category->currentpage() }}">
+                                <input type="hidden" name="perpage">
                                 @foreach ($category as $cat)
-                                    <tr>
-                                        <td>{{$cat->id }}</td>
+                                    <tr class="data">
+                                        <td>{{ $loop->index + 1 }}</td>
+                                        {{-- <td>{{ ($cat->currentpage() - 1) * $cat->perpage() + $loop->index + 1 }}</td> although, methods of pagination are not working in FOREACH (just an idea)--}}
                                         <td>{{ $cat->name }}</td>
                                         <td>{{ $cat->note }}</td>
                                         <td><i class="fa-solid fa-pencil" title="Edit"></i></td>
@@ -51,6 +53,7 @@
                         {{ $category->appends(request()->query())->links() }}
                     </div>
                 </div>
-            </div>
         </div>
+@endsection
+@section('scriptSource')
 @endsection
--- resources/views/mainLayout.blade.php
+++ resources/views/mainLayout.blade.php
@@ -5,7 +5,6 @@
     <meta charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link rel="stylesheet" href="index.css">
     <!-- bootstrap -->
     <!-- CSS only -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
@@ -24,44 +23,46 @@
 <body>
     <div class="container-fluid m-1">
         <div class="row shadow-sm sticky-top bg-white">
+         {{-- offcanvas outside --}}
+            <div class="offcanvas offcanvas-start" data-bs-scroll="true" data-bs-backdrop="false"
+            tabindex="-1" id="offcanvasScrolling" aria-labelledby="offcanvasScrollingLabel">
+            <div class="offcanvas-header">
+                <a href="{{ route('Auth@dashboard') }}" class=" text-decoration-none {{ "dashboard" == request()->path() ? "active-nav" : "text-dark" }}"><h5 class="offcanvas-title" id="offcanvasScrollingLabel"><i class="fa-solid fa-house me-2"></i>Tackle Shop</h5></a>
+                <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+            </div>
+            <div class="offcanvas-body border-top">
+                <ul class=" list-unstyled list-group list-group-flush">
+                    <a href="{{ route('Auth@categoryPage') }}" class=" text-decoration-none list-group-item list-group-item-action rounded {{ "category" == request()->path() ? "active-nav" : "" }}">
+                        <li><i class="fa-solid fa-cubes-stacked me-2"></i>Category</li>
+                    </a>
+                    <a href="{{ route('Auth@postPage') }}" class=" text-decoration-none list-group-item list-group-item-action rounded {{ "post" == request()->path() ? "active-nav" : "" }}">
+                        <li><i class="fa-solid fa-pen-to-square me-2"></i>Post</li>
+                    </a>
+                    <a href="{{ route('Auth@overViewPage') }}" class=" text-decoration-none list-group-item list-group-item-action rounded {{ "overView" == request()->path() ? "active-nav" : "" }}">
+                        <li><i class="fa-solid fa-eye me-2"></i>OverViews</li>
+                    </a>
+                    <a href="{{ route('Auth@adminListPage') }}" class=" text-decoration-none list-group-item list-group-item-action rounded {{ "adminList" == request()->path() ? "active-nav" : "" }}">
+                        <li><i class="fa-solid fa-users me-2"></i>Admins</li>
+                    </a>
+                    <!-- <span class=" list-group-item list-group-item-action"> -->
+                    <form action="{{ route('logout') }}" method="post">
+                     @csrf
+                      <button class="btn btn-sm btn-primary mt-3" type="submit">Logout</button>
+                    </form>
+
+                    <!-- </span> -->
+
+                </ul>
+            </div>
+
+        </div>
+        {{-- end of offcanvas outside --}}
             <div class="col-1">
-                <!-- offcanvas -->
+                <!-- offcanvas button -->
                 <button class="btn btn-secondary btn-sm mb-2" type="button" data-bs-toggle="offcanvas"
                     data-bs-target="#offcanvasScrolling" aria-controls="offcanvasScrolling"><i
                         class="fa-solid fa-bars"></i></button>
-
-                <div class="offcanvas offcanvas-start w-25" data-bs-scroll="true" data-bs-backdrop="false"
-                    tabindex="-1" id="offcanvasScrolling" aria-labelledby="offcanvasScrollingLabel">
-                    <div class="offcanvas-header">
-                        <a href="{{ route('Auth@dashboard') }}" class=" text-decoration-none {{ "dashboard" == request()->path() ? "active-nav" : "text-dark" }}"><h5 class="offcanvas-title" id="offcanvasScrollingLabel"><i class="fa-solid fa-house me-2"></i>Tackle Shop</h5></a>
-                        <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
-                    </div>
-                    <div class="offcanvas-body border-top">
-                        <ul class=" list-unstyled list-group list-group-flush">
-                            <a href="{{ route('Auth@categoryPage') }}" class=" text-decoration-none list-group-item list-group-item-action rounded {{ "category" == request()->path() ? "active-nav" : "" }}">
-                                <li><i class="fa-solid fa-cubes-stacked me-2"></i>Category</li>
-                            </a>
-                            <a href="{{ route('Auth@postPage') }}" class=" text-decoration-none list-group-item list-group-item-action rounded {{ "post" == request()->path() ? "active-nav" : "" }}">
-                                <li><i class="fa-solid fa-pen-to-square me-2"></i>Post</li>
-                            </a>
-                            <a href="{{ route('Auth@overViewPage') }}" class=" text-decoration-none list-group-item list-group-item-action rounded {{ "overView" == request()->path() ? "active-nav" : "" }}">
-                                <li><i class="fa-solid fa-eye me-2"></i>OverViews</li>
-                            </a>
-                            <a href="{{ route('Auth@adminListPage') }}" class=" text-decoration-none list-group-item list-group-item-action rounded {{ "adminList" == request()->path() ? "active-nav" : "" }}">
-                                <li><i class="fa-solid fa-users me-2"></i>Admins</li>
-                            </a>
-                            <!-- <span class=" list-group-item list-group-item-action"> -->
-                            <form action="{{ route('logout') }}" method="post">
-                             @csrf
-                              <button class="btn btn-sm btn-primary mt-3" type="submit">Logout</button>
-                            </form>
-
-                            <!-- </span> -->
-
-                        </ul>
-                    </div>
-                </div>
-                <!-- end of offcanvas -->
+                <!-- end of offcanvas button -->
             </div>
             <div class="col">
                 <div class="row">
@@ -79,7 +80,7 @@
                     <!-- end of Search --> --}}
                     <div class="col-4 offset-4 my-auto fs-4">Tackle Shop</div>
                     <div class="col-1 offset-3">
-                        <img src="./img/five.jpeg" alt="" class=" my-1 shadow-sm img-thumbnail"
+                        <img src="" alt="" class=" my-1 shadow-sm img-thumbnail"
                             style=" height: 40px; border-radius: 50%;">
                     </div>
                 </div>
@@ -93,6 +94,6 @@
 <!-- JavaScript Bundle with Popper -->
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
     integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous">
-    </script>
-
+</script>
+@yield('scriptSource')
 </html>
